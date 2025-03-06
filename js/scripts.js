678e4dbcc87a33f48010649c27808d1ef1c4d44a
AHenry95 <ahenry.dev@gmail.com>
2025-03-06 14:25:26 -0500

    Add pokemonList array and pokemon data
    - removed existing scripts in scripts.js
    - created new array named pokemonList
     - added objects for three pokemon to the list, with keys for height, name, and type.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
-alert('Hello world');
-
-let favoriteFood = 'Pizza';
-document.write(favoriteFood); 
+let pokemonList = [ 
+    {
+        name: 'Bulbasaur', 
+        height: 7,
+        type: ['grass', 'poison']
+    }, 
+    {
+        name: 'Charmander', 
+        height: 6, 
+        type: ['fire']
+    },
+    {
+        name: 'Squirtle', 
+        height: 5, 
+        type: ['water']
+    }
+];
