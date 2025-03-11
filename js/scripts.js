fdb79e27ad3598a6bd8eb5cb8e3b741e68a9f987
AHenry95 <ahenry.dev@gmail.com>
2025-03-11 10:50:39 -0400

    Add loop to scripts.js
    - Added for loops to display names and heights of pokemon contained in pokemonList array
    - two versions of loop includes, Version A commented out
    - Changed heights of pokemen
    - Added Charizard object to pokemon.List array

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,47 @@
 let pokemonList = [ 
     {
         name: 'Bulbasaur', 
-        height: 7,
+        height: 0.7,
         type: ['grass', 'poison']
     }, 
     {
         name: 'Charmander', 
-        height: 6, 
+        height: 0.6, 
         type: ['fire']
     },
     {
+        name: 'Charizard', 
+        height: 1.7, 
+        type: ['fire', 'flying']
+    },
+    {
         name: 'Squirtle', 
-        height: 5, 
+        height: 0.5, 
         type: ['water']
     }
 ];
+
+/* Version A - My inital attempt
+
+for (i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height > 1) {
+        document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ' + '- Wow, that\'s big!')
+    } else { 
+        document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ');
+    }
+} */
+
+// Version B 
+
+
+for (i = 0; i < pokemonList.length; i++) {
+    let pokemon = pokemonList[i];
+
+    document.write(`${pokemon.name} (height: ${pokemon.height})`);
+
+    if(pokemon.height > 1) {
+        document.write(' - Wow, that\'s big!');
+    }
+
+    document.write('<br>');
+} 
